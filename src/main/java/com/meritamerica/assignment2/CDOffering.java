4e772233a190898ba8b50a773ebd157e3ae82225
reubensoap <34084097+reubensoap@users.noreply.github.com>
2020-03-05 03:52:46 -0600

    Add files via upload

--- src/main/java/com/meritamerica/assignment2/CDOffering.java
+++ src/main/java/com/meritamerica/assignment2/CDOffering.java
@@ -1,5 +1,30 @@
 package com.meritamerica.assignment2;
 
-public class CDOffering {
+/*
+ * @author Reuben Pena
+ * @date 3/5/2020
+ * @description CDOffering class used to store information
+ * regarding CDOffering methods / variables
+ */
 
+public class CDOffering {
+    private int term;
+    private double interestRate;
+
+    public CDOffering(int term, double interestRate){
+        this.term = term;
+        this.interestRate = interestRate;
+    }
+
+    public int getTerm(){
+        return this.term;
+    }
+
+    public double getInterestRate(){
+        return this.interestRate;
+    }
+
+    public String toString(){
+        return "Term: " + this.getTerm() + " Interest Rate: " + this.getInterestRate();
+    }
 }
--- src/main/java/com/meritamerica/assignment2/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment2/SavingsAccount.java
@@ -1,6 +1,90 @@
 package com.meritamerica.assignment2;
 
-public class SavingsAccount {
+import java.text.DecimalFormat;
+import java.util.Random;
 
+/*
+ * @author Reuben Pena
+ * @date 3/5/2020
+ * @description Savings Account Class used to store information for the account
+ * holder in regards to saving account information.
+ */
+
+public class SavingsAccount {
+    // Variables
+    private double balance;
+    private static double interest = 0.01;
+    private long accountNumber = 0;
+
+    //Constructors
+    public SavingsAccount(){
+        this.balance = 0.0;
+    }
+
+    public SavingsAccount(double savingB){
+        this.balance = savingB;
+        this.accountNumber = (long)(Math.random() * 100000);
+    }
+
+    //Getters and Setters
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public double getInterestRate(){
+        return interest;
+    }
+
+    public long getAccountNumber(){
+        return this.accountNumber;
+    }
+
+    //Methods
+
+    // Stores future values of savings account with interest over a
+    // set amount of years.
+    public double futureValue(int years){
+        double pv = this.getBalance();
+        double fv = pv * (Math.pow((1 + this.getInterestRate()), years));
+        return fv;
+    }
+
+    // Used to determine if the withdraw amount is not a negative number
+    // or puts the checking account balance under $0.00 and then
+    // takes away amount from savings account balance.
+    public boolean withdraw(double amount){
+        if (amount < 0){
+            System.out.println("Unable to withdraw a negative amount. Try again.");
+            return false;
+        } else {
+            if((this.getBalance() - amount) < 0){
+                System.out.println("Not enough funds in the account.");
+                return false;
+            } else {
+                double newB = this.getBalance() - amount;
+                this.balance = newB;
+                return true;
+            }
+        }
+    }
+
+    // Used to determine if the deposit amount is not a negative number
+    // and then adds amount to savings account balance
+    public boolean deposit(double amount){
+        if(amount <= 0){
+            System.out.println("Unable to deposit negative funds. Try again.");
+            return false;
+        } else {
+            this.balance = this.getBalance() + amount;
+            return true;
+        }
+    }
+
+    public String toString(){
+        DecimalFormat format = new DecimalFormat("##.00");
+        return "\nSaving Account Balance: $" + format.format(this.getBalance()) + "\n"
+                + "Saving Account Interest Rate: " + this.getInterestRate() + "\n"
+                + "Saving Account Balance in 3 years: $" + format.format(this.futureValue(3));
+    }
 
 }
